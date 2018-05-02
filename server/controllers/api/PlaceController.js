f8554c666980e3f4c9bf28dcb960c34bf0749eec
Phong Le T <phong.le@asiantech.vn>
2018-05-02 13:51:15 +0700

    save

--- server/controllers/api/PlaceController.js
+++ server/controllers/api/PlaceController.js
@@ -2,7 +2,7 @@ import HttpStatus from 'http-status';
 import Response from '../../helpers/Response'
 import PlaceRepository from '../../repositories/PlaceRepository'
 import CategoryRepository from '../../repositories/CategoryRepository'
-import {category, hotel, location, restaurant, touristattraction,comment} from '../../models/index';
+import {category, comment, hotel, location, restaurant, touristattraction} from '../../models/index';
 
 const DB = require('../../config/db-config.json');
 const connection = DB['development'];
@@ -15,8 +15,7 @@ const categoryRepository = new CategoryRepository();
 class PlaceController {
     index = async (req, res) => {
         try {
-            let places = await placeRepository.find({
-            });
+            let places = await placeRepository.find({});
             console.log(places)
             return Response.success(res, places);
         }
@@ -24,7 +23,7 @@ class PlaceController {
             return Response.error(res, e, HttpStatus.BAD_REQUEST);
         }
     };
-    create = async(req, res) => {
+    create = async (req, res) => {
         try {
             //viet ham insert o day
             let data = req.body;
@@ -36,16 +35,16 @@ class PlaceController {
         }
     }
     update = async (req, res) => {
-        try{
+        try {
             let placeId = req.param('id');
             let data = req.body;
-            let returnData = await placeRepository.update(returnData,{
+            let returnData = await placeRepository.update(returnData, {
                 where: {
-                    id : placeId
+                    id: placeId
                 }
             });
             return Response.success(res, returnData);
-        }catch(e){
+        } catch (e) {
             Response.error(res, e, HttpStatus.BAD_REQUEST)
         }
     }
@@ -53,19 +52,19 @@ class PlaceController {
         try {
             let placeIdReq = req.param('id');
             let places = await placeRepository.find({
-                where:{
-                    id : placeIdReq
+                where: {
+                    id: placeIdReq
                 },
-                include:[{
-                    model : category,
-                    attributes :['categoryName'],
-                },{
+                include: [{
+                    model: category,
+                    attributes: ['categoryName'],
+                }, {
                     model: restaurant
-                },{
+                }, {
                     model: touristattraction
-                },{
+                }, {
                     model: hotel
-                },{
+                }, {
                     model: comment
                 }]
             });
@@ -75,6 +74,51 @@ class PlaceController {
             return Response.error(res, e, HttpStatus.BAD_REQUEST)
         }
     };
+    viewTopPlace = async (req, res) => {
+        try {
+            let places = await placeRepository.find({
+                orderby: 'rating', limit: 2,
+                include: [{
+                    model: category,
+                    attributes: ['categoryName'],
+                }, {
+                    model: restaurant
+                }, {
+                    model: touristattraction
+                }, {
+                    model: hotel
+                }]
+            });
+            return Response.success(res, places);
+        } catch (e) {
+            Response.error(res, e, HttpStatus.BAD_REQUEST);
+        }
+    };
+    viewTopByCategoryId = async (req, res) => {
+        try {
+            let categoryIdReq = req.param('categoryId');
+            let places = await placeRepository.find({
+                where: {
+                    categoryId: categoryIdReq
+                },
+                orderby: 'rating', limit: 2,
+                include: [{
+                    model: category,
+                    attributes: ['categoryName'],
+                }, {
+                    model: restaurant
+                }, {
+                    model: touristattraction
+                }, {
+                    model: hotel
+                }]
+            });
+            return Response.success(res, places);
+        } catch (e) {
+            Response.error(res, e, HttpStatus.BAD_REQUEST);
+        }
+    };
+
     viewByLocation = async (req, res) => {
         try {
             let locationIdReq = req.param('locationId');
@@ -84,7 +128,7 @@ class PlaceController {
                     locationId: locationIdReq
                 },
                 include: {
-                    model : location,
+                    model: location,
                     attributes: ['locationName']
                 }
             });
@@ -101,13 +145,42 @@ class PlaceController {
                 where: {
                     categoryId: categoryIdReq
                 },
-                include :{
-                    model : category,
-                    attributes : ['categoryName']
+                include: {
+                    model: category,
+                    attributes: ['categoryName']
                 }
             });
             return Response.success(res, places)
         } catch (e) {
+            return Response.error(res, e, HttpStatus.BAD_REQUEST);
+        }
+    };
+    searchByName = async (req, res) => {
+        try {
+            let query = req.body.query;
+            let places = await placeRepository.find({
+                where: {
+                    placeName: {
+                        ilike: '%' + query + '%'
+                    }
+                },
+                include: [{
+                        model: category,
+                        attributes: ['categoryName']
+                    },
+                    {
+                        model: restaurant
+                    }, {
+                        model: hotel
+                    }, {
+                        model: touristattraction
+                    }, {
+                        model: comment
+                    }]
+            });
+            return Response.success(res, places);
+        }
+        catch (e) {
             return Response.error(res, e, HttpStatus.BAD_REQUEST);
         }
     };
--- server/routes/api/PlaceRouter.js
+++ server/routes/api/PlaceRouter.js
@@ -5,8 +5,11 @@ const router = Router();
 
 router.route('').get(PlaceController.index);
 router.route('').post(PlaceController.create)
-router.route('/:id').get(PlaceController.viewByPlaceId);
+router.route('/viewById/:id').get(PlaceController.viewByPlaceId);
 router.route('/viewByLocation/:locationId').get(PlaceController.viewByLocation);
 router.route('/viewByCategory/:categoryId').get(PlaceController.viewByCategory);
+router.route('/viewTop').get(PlaceController.viewTopPlace);
+router.route('/viewTopByCategory/:categoryId').get(PlaceController.viewTopByCategoryId);
+router.route('/search/').post(PlaceController.searchByName);
 
 export default router;
